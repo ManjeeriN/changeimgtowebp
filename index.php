03d390fc41bb08a284ad42f4480218e6321cc6ff
Manjeeri Nawghare <40880091+ManjeeriN@users.noreply.github.com>
2020-07-08 11:39:17 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+    <h1 style="text-align:center;">Convert JPG/PNG Image to WEBP Using PHP</h1>
+    <?php
+    $file = 'image.png';
+    $image = imagecreatefromstring(file_get_contents($file));
+    ob_start();
+    imagejpeg($image,NULL,100);
+    $cont = ob_get_contents();
+    ob_end_clean();
+    imagedestroy($image);
+    $content = imagecreatefromstring($cont);
+    $output = 'newimage.webp';
+    imagewebp($content,$output);
+    imagedestroy($content);
+    echo '<h4>Output Image Saved as '.$output.'</h4>';
+    ?>
